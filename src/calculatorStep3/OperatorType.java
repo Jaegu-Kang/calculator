27d0a86a021d8889cadc9410f71e6beeecec8d77
Jaegu-Kang <rkdworn8@gmail.com>
2026-03-25 12:31:26 +0900

    계산기 step3 OperatorType.java 람다식 작성

--- src/calculatorStep3/OperatorType.java
+++ src/calculatorStep3/OperatorType.java
@@ -1,15 +1,23 @@
 package calculatorStep3;
 
+import java.util.function.BiFunction;
+
 public enum OperatorType {
-    SUM("+"),
-    SUB("-"),
-    MUL("*"),
-    DIV("/");
+    SUM("+", (a, b) -> a + b),
+    SUB("-", (a, b) -> a - b),
+    MUL("*", (a, b) -> a * b),
+    DIV("/", (a, b) -> a / b);
 
     private final String type;
+    private final BiFunction<Integer, Integer, Integer> oper;
 
-    OperatorType(String type) {
+    OperatorType(String type, BiFunction<Integer, Integer, Integer> oper) {
         this.type = type;
+        this.oper = oper;
     }
-    public String getType() {return type;}
+    public String getType()
+    {return type;}
+    public int cal(int a, int b) {
+        return oper.apply(a, b);
+    }
 }
