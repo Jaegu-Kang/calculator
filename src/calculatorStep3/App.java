8cfedbd0f72a675fcb0969f90d9c8b38ecf8c5e1
Jaegu-Kang <rkdworn8@gmail.com>
2026-03-25 16:40:38 +0900

    계산기 step3 스트림 사용을 위한 코드수정, 주석 수정

--- src/calculatorStep3/App.java
+++ src/calculatorStep3/App.java
@@ -20,6 +20,7 @@ public class App {
             System.out.println("종료:      exit      입력");
             System.out.println("기록확인:   record    입력");
             System.out.println("기록삭제:   remove    입력");
+            System.out.println("기록검색:   search    입력");
             System.out.print("원하는 메뉴를 입력하세요:");
             // 사용자가 입력한 문자열을 읽어 옵니다.
             String menu = sc.next();
@@ -43,6 +44,13 @@ public class App {
                 case "remove":
                     cal.remove();
                     break;
+                // 입력한 값보다 큰 값을 겁색하는 기능
+                case "search":
+                    double thisNum = sc.nextDouble();
+                    System.out.println("검색 결과는: ");
+                    cal.resultThan(thisNum);
+                    break;
+
                 // 입력 받은 문자가 사칙 연산 기록일때
                 case "+", "-", "*", "/" :
                     // 첫번째 숫자를 입력 받습니다.
--- src/calculatorStep3/ArithmeticCalculator.java
+++ src/calculatorStep3/ArithmeticCalculator.java
@@ -7,6 +7,10 @@ public class ArithmeticCalculator<T extends Number> {
 
     // 계산 기록을 저장하는 리스트
     private ArrayList<String> record = new ArrayList<>();
+
+    // 스트림 사용을 위한 계산 결과값만 저장하는 리스트
+    private ArrayList<Double> results = new ArrayList<>();
+
     // enum을 활용하여 연산 메서드를 통합
     // 매개변수 a, b를 타입 T로 변경, 반환타입을 double로 변경: 나중에 발견
     public double calculate(T a, T b, OperatorType type) {
@@ -18,11 +22,22 @@ public class ArithmeticCalculator<T extends Number> {
         // enum에게 a, b를 넘겨줍니다.
         double result = type.cal(a1, b1);
 
-        // 기록을 저장합니다.
+        // 계산기록 전체를 저장합니다.
         record.add(a + type.getType() + b +" = " + result);
 
+        // 스트림활용을 위해 계산 결과 값만 저장합니다.
+        results.add(result);
 
         return result;
+    }
+
+    // 스트림을 활용한 조회 메서드
+    public void resultThan(double thisNum) {
+        // 계산 결과 값을 스트림에 호출합니다.
+        results.stream()
+                // 입력한 값 thisNum 보다 큰값만 걸러내고 그 값을 출력합니다.
+                .filter(result -> result > thisNum)
+                .forEach(result -> System.out.println(result));
     }
 
     // 누적된 계산 기록을 반환합니다.
